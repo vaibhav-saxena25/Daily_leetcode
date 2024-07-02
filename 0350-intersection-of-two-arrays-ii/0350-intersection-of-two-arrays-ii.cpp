723ac15a69c16bb2c12e93eac426765377655e06
vaibhav-saxena25 <149486002+vaibhav-saxena25@users.noreply.github.com>
2024-07-02 21:06:00 +0530

    Time: 9 ms (11.77%), Space: 14.2 MB (21.98%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> count;
-        vector<int> answer;
-
-        // Count elements in nums1
-        for (int num : nums1) {
-            count[num]++;
+        unordered_map<int,int>mp;
+        for(int &num:nums1){
+            mp[num]++;
         }
-
-        // Find intersections with nums2
-        for (int num : nums2) {
-            if (count[num] > 0) {
-                answer.push_back(num);
-                count[num]--;
+        vector<int>result;
+        for(int i =0;i<nums2.size();i++){
+            int element=nums2[i];
+            if(mp[element]>0){
+                mp[element]--;
+                result.push_back(element);
             }
         }
-
-        return answer;
+        return result;
+                
     }
 };
