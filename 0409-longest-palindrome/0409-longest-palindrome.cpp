e3ac4cdd7ec9b7d05d58e6986e0ef121818530ab
vaibhav-saxena25 <149486002+vaibhav-saxena25@users.noreply.github.com>
2024-07-02 23:21:45 +0530

    Time: 6 ms (23.76%), Space: 10 MB (8.71%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_set<char>st;
+        int result=0;
+        for(char &ch:s){
+            if(st.count(ch)){
+                result+=2;
+                st.erase(ch);
+            }else{
+                st.insert(ch);
+            }
+        }
+        if(!st.empty()){
+            result++;
+        }
+        return result;
+    }
+};
